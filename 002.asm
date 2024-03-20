55d67220d7a24b5f41d94e41da8507dd82415e9c
INIT-REF <rufmichan@freenet.de>
2024-03-20 10:07:55 +0100

    Update

---
+++ 002.asm
@@ -0,0 +1,35 @@
+section .data
+    msg db "%d", 10, 0              ;return string for printf (just the result)
+
+section .text
+extern printf
+global main
+
+main:
+    xor     eax, eax    ;result
+    mov     ebx, 1      ;fib (n-1)
+    mov     ecx, 2      ;fib (n)
+
+fib:
+    test    ecx, 1      ;check if last bit is 1 (number is odd)
+    jnz     odd         ;if odd, jump to odd
+    add     eax, ecx    ;if even, add to result
+
+odd:
+    xadd    ecx, ebx        ;exchange ebx and ecx, put the sum in ecx
+    cmp     ecx, 4000000    ;check if we are still under 4 million
+    jl      fib             ;if yes, continue
+
+print:                      ;printing routine, differs slightly from OS to OS
+    push    rbp
+    mov     edi, msg
+    mov     esi, eax
+    call    printf
+    pop     rbp
+
+exit:                       ;exit routine, dito
+    mov     eax, 1
+    xor     edi, edi
+    syscall
+
+section .note.GNU-stack     ;just for gcc
