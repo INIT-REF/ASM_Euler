e5c29de0f113bfa7b64507055aad4f9a8930fc4b
INIT-REF <rufmichan@freenet.de>
2024-04-02 06:50:44 +0200

    Update

--- 009.asm
+++ 009.asm
@@ -1,14 +1,12 @@
 section .data
-    msg db "%d", 10, 0              ;return string for printf (just the result)
+    msg db "%d", 10, 0  ;return string for printf (just the result)
 
 section .text
     extern printf
     global main
 
 main:
-    xor     eax, eax
     xor     ebx, ebx    ;r in Dickson's method
-    xor     ecx, ecx    ;r^2 / 2 in Dickson's method
 
 next:
     add     ebx, 2      ;the next even r (odd values not allowed)
@@ -29,22 +27,19 @@ triplets:
     div     edi         ;divide by current divisor
     test    edx, edx    ;check if remainder is zero
     jnz     triplets    ;if not, continue with next divisor
-    mov     edx, ebx    ;a = r
-    add     edx, edi    ;a = r + s
-    mov     esi, ebx    ;b = r
-    add     esi, eax    ;b = r + t
-    mov     eax, esi    ;c = r + t
-    add     eax, edi    ;c = r + s + t
-    push    rdx         ;push rdx on the stack for later use
-    push    rax         ;push rax on the stack, dito
-    add     eax, esi    ;perimeter
-    add     eax, edx
-    cmp     eax, 1000   ;check if perimeter = 1000
+    mov     r8d, ebx    ;a = r
+    add     r8d, edi    ;a = r + s
+    mov     r9d, ebx    ;b = r
+    add     r9d, eax    ;b = r + t
+    mov     r10d, r9d   ;c = r + t
+    add     r10d, edi   ;c = r + s + t
+    mov     eax, r8d    ;put a in eax for product
+    add     r8d, r9d    ;perimeter
+    add     r8d, r10d
+    cmp     r8d, 1000   ;check if perimeter = 1000
     jne     triplets    ;if not, continue with next candidate
-    pop     rax         ;if yes, compute a * b * c
-    mul     esi
-    pop     rdx
-    mul     edx
+    mul     r9d         ;calculate product
+    mul     r10d
 
 print:                  ;printing routine, differs slightly from OS to OS
     push    rbp
@@ -53,9 +48,9 @@ print:                  ;printing routine, differs slightly from OS to OS
     call    printf
     pop     rbp
 
-exit:                       ;exit routine, dito
+exit:                   ;exit routine, dito
     mov     eax, 1
     xor     edi, edi
     syscall
 
-section .note.GNU-stack     ;just for gcc
+section .note.GNU-stack ;just for gcc
