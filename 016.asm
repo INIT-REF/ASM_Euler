23b932d17fbecb5416c58b5fcecdaaf4e52a9ef5
INIT-REF <rufmichan@freenet.de>
2024-04-03 14:22:24 +0200

    Update

--- 016.asm
+++ 016.asm
@@ -1,6 +1,6 @@
 section .data
     msg db "%d", 10, 0              ;return string for printf (just the result)
-    num times 303 dd 0
+    num times 302 dd 0
 
 section .text
     extern printf
@@ -8,14 +8,14 @@ section .text
 
 main:
     xor     ebx, ebx                    ;carry
-    mov     edi, 302                    ;array index
+    mov     edi, 301                    ;array index
     mov     ecx, 10                     ;for divisions
     mov     esi, 1000                   ;power counter
     mov     dword [num + 4 * edi], 1    ;initial state
 
 power:
     dec     esi                         ;decrease counter
-    mov     edi, 302                    ;reset index
+    mov     edi, 301                    ;reset index
 
 multiply:
     xor     edx, edx                    ;reset remainder
@@ -26,8 +26,8 @@ multiply:
     mov     ebx, eax                    ;carry = result
     mov     dword [num + 4 * edi], edx  ;digit @ edi = remainder
     dec     edi                         ;decrease index
-    test    edi, edi                    ;check if index = 0
-    jnz     multiply                    ;if not, repeat
+    cmp     edi, 0                    ;check if index = 0
+    jge     multiply                    ;if not, repeat
     test    esi, esi                    ;check if completed
     jnz     power                       ;if not, jump to power
     xor     eax, eax                    ;else reset eax and ebx
@@ -36,7 +36,7 @@ multiply:
 sum:                                    ;sum all digits
     add     eax, [num + 4 * ebx]
     inc     ebx
-    cmp     ebx, 303
+    cmp     ebx, 302
     jl      sum
     
 print:                      ;printing routine, differs slightly from OS to OS
