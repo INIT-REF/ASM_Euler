399e0abcb24be111890769aa5ea121cd9af3dbdd
INIT-REF <rufmichan@freenet.de>
2024-04-12 20:07:45 +0200

    Update

--- 041.asm
+++ 041.asm
@@ -1,27 +1,27 @@
 section .data
-    msg db "%d", 10, 0          ;return string for printf (just the result)
-    abc times 8 db 0           ;digits in number
-    pds db "1234567", 1       ;pandigital string
+    msg db "%d", 10, 0  ;return string for printf (just the result)
+    abc times 9 db 0    ;digits in number
+    pds db "1234567", 1 ;pandigital string
 
 section .text
     extern printf
     global main
 
 main:
-    mov     edi, 10                 ;for divisions
-    mov     r10d, 7654321           ;upper limit in r10d
+    mov     edi, 10         ;for divisions
+    mov     ebx, 7654321    ;upper limit
 
 next:
-    sub     r10d, 2                 ;next lower odd number
-    mov     esi, 1                  ;divisor for prime test
+    sub     ebx, 2      ;next lower odd number
+    mov     esi, 1      ;divisor for prime test
 
 primetest:
     add     esi, 2      ;next odd divisor
     mov     eax, esi    ;square divisor
     mul     esi
-    cmp     eax, r10d   ;check if divisor > number
+    cmp     eax, ebx    ;check if divisor^2 > number
     jg      isprime     ;if yes, we have a prime
-    mov     eax, r10d   ;number in eax for division
+    mov     eax, ebx    ;number in eax for division
     xor     edx, edx    ;reset remainder
     div     esi         ;divide by current divisor
     test    edx, edx    ;is remainder 0?
@@ -29,12 +29,12 @@ primetest:
     jmp     next        ;else check next number
 
 isprime:
-    mov     eax, r10d   ;number in eax
+    mov     eax, ebx    ;number in eax
     push    rax         ;put number on the stack
     call    digits      ;digits in abc and compare with pds
     pop     rax         ;get number back
-    cmp     esi, 8    ;is abc 1-n pandigital?
-    jne     next        ;if not, try next prime
+    cmp     esi, 8      ;is abc 1-7 pandigital?
+    jl      next        ;if not, try next prime
 
 print:                      ;printing routine, differs slightly from OS to OS
     push    rbp
@@ -72,8 +72,6 @@ cmpstr:
     inc     esi                 ;next char
     cmp     r8b, r9b            ;both characters match?
     je      cmpstr              ;if yes, continue
-
-back:
     ret
 
 section .note.GNU-stack     ;just for gcc
