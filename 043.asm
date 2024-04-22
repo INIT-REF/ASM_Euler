6a21e1cf94207ed3a68209a8b91cb81d2903f81a
INIT-REF <rufmichan@freenet.de>
2024-04-22 05:59:40 +0200

    Update

--- 043.asm
+++ 043.asm
@@ -1,61 +1,96 @@
 section .data
-    msg db "%s", 10, 0              ;return string for printf (just the result)
-    string db "1023456789"
-    result db "0000000000"
-    fac dd 362880, 40320, 5040, 720, 120, 24, 6, 2, 1, 1    ;9! ... 0!
+    msg db "%lld", 10, 0              ;return string for printf (just the result)
+    num dd 1, 0, 2, 3, 4, 5, 6, 7, 8, 9 ;first perutation
+    divs dd 2, 3, 5, 7, 11, 13, 17      ;divisors
    
 section .text
     extern printf
     global main
 
 main:
-    xor     eax, eax          ;number of permutation (original is 1st)
-
-next:
-    inc     eax
-    push    rax
-    xor     ebx, ebx          ;index for factorials
-
-permute:
-    cmp     ebx, 10                     ;finished?
-    je      print
-    xor     edx, edx                    ;reset remainder
-    mov     dword ecx, [fac + 4 * ebx]  ;ebx!
-    div     ecx                         ;divide eax by ebx!
-    mov     sil, [string + eax]         ;copy digit @ eax
-    mov     [result + ebx], sil         ;move digit to result @ ebx
-    inc     ebx                         ;next factorial
-    mov     edi, eax                    ;copy index of digit to edi
-    mov     eax, edx                    ;remainder = next dividend
-
-updatestring:
-    inc     edi                         ;next digit of string
-    mov     sil, [string + edi]         ;copy to sil
-    dec     edi                         ;decrease index
-    mov     [string + edi], sil         ;move digit there
-    inc     edi                         ;increase index again
-    cmp     edi, 10                     ;finished?
-    jl      updatestring                ;if not, repeat
-    jmp     permute                     ;else continue with permute
-    pop     rax
+    xor     esi, esi            ;setup some registers
+    xor     rcx, rcx
+    
+reset:
+    xor     rax, rax
+    xor     edi, edi
+    call    convert             ;convert current permutation to actual number
+    mov     rbx, 9876543210     ;last permutation reached?
+    cmp     rax, rbx
+    jge     print               ;if yes, print result
+    xor     rax, rax            ;else reset some registers
+    xor     rbx, rbx
     mov     edi, 1
-    xor     r8d, r8d
+    xor     esi, esi
 
-getdigits:
-    mov     r8b, [result + edi]
-    mov     r9b, [result + edi + 1]
-    mov     r10b, [result + edi + 2]
-    imul    r8d, 100
-    imul    r9d, 10
-    add     r8d, r9d
-    add     r8d, r10d
-    ret
-      
+divisortest: 
+    mov     eax, [num + 4 * edi]    ;extract the three-digit number
+    imul    eax, 10
+    inc     edi
+    add     eax, [num + 4 * edi]
+    imul    eax, 10
+    inc     edi
+    add     eax, [num + 4 * edi]
+    mov     ebx, [divs + 4 * esi]
+    xor     edx, edx                ;reset remainder
+    div     ebx                     ;divide by divs @ esi
+    test    edx, edx                ;check remainder
+    jnz     nextperm                ;if not zero, try next permuation
+    dec     edi                     ;else setup for next test
+    inc     esi
+    cmp     esi, 7                  ;last divisor reached?
+    je      addnum                  ;if yes, add current permutation to result
+    jmp     divisortest             ;else continue divisor test
+
+addnum:
+    xor     edi, edi    ;prepare registers for conversion
+    xor     rax, rax
+    call    convert     ;call convert function
+    add     rcx, rax    ;add permutation to result
+
+nextperm:
+    mov     edi, 9      ;set up registers for next permutation
+    mov     esi, 10
+    xor     rax, rax
+    xor     rbx, rbx
+
+findpivot:                          ;find the "pivot"
+    dec     edi
+    mov     eax, [num + 4 * edi]
+    inc     edi
+    mov     ebx, [num + 4 * edi]
+    dec     edi
+    cmp     eax, ebx
+    jg      findpivot
+
+findceil:                           ;find the "ceiling"
+    dec     esi
+    mov     eax, [num + 4 * esi]
+    cmp     eax, [num + 4 * edi]
+    jl      findceil
+
+swap:                               ;swap pivot and ceiling
+    mov     ebx, [num + 4 * edi]
+    mov     [num + 4 * edi], eax
+    mov     [num + 4 * esi], ebx
+    inc     edi
+    mov     esi, 9
+
+reverse:                            ;reverse substring after pivot
+    cmp     edi, esi
+    jge     reset
+    mov     eax, [num + 4 * edi]
+    mov     ebx, [num + 4 * esi]
+    mov     [num + 4 * edi], ebx
+    mov     [num + 4 * esi], eax
+    inc     edi
+    dec     esi
+    jmp     reverse
 
 print:                      ;printing routine, differs slightly from OS to OS
     push    rbp
     mov     edi, msg
-    mov     esi, result
+    mov     rsi, rcx
     call    printf
     pop     rbp
 
@@ -63,5 +98,14 @@ exit:                       ;exit routine, dito
     mov     eax, 1
     xor     edi, edi
     syscall
+
+convert:                            ;convert current permutation to number
+    imul    rax, 10
+    mov     ebx, [num + 4 * edi]
+    add     rax, rbx
+    inc     edi
+    cmp     edi, 10
+    jl      convert
+    ret
 
 section .note.GNU-stack     ;just for gcc
