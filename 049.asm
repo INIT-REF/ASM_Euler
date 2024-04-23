74e6eff6b071ad791aa7d572c68a9e0f6572d9ce
INIT-REF <rufmichan@freenet.de>
2024-04-23 14:23:25 +0200

    Update

--- 049.asm
+++ 049.asm
@@ -40,18 +40,13 @@ nextstarter:
     cmp     byte [primes + eax], 1  ;is number prime?
     jne     nextstarter             ;if not, try next
     mov     ebx, eax                ;else set ebx
-
-nextfollowers:
-    inc     ebx                     ;next second number
+    add     ebx, 3330               ;and add 3330
     cmp     byte [primes + ebx], 1  ;is it prime?
-    jne     nextfollowers           ;if not, try next
+    jne     nextstarter             ;if not, try next
     mov     ecx, ebx                ;else put it in ecx
-    sub     ecx, eax                ;and subtract eax
-    add     ecx, ebx                ;add ebx to difference
-    cmp     ecx, 10000              ;result > 9999?
-    jge     nextstarter             ;if yes, try next starter
+    add     ecx, 3330               ;and add 3330
     cmp     byte [primes + ecx], 1  ;is third number prime?
-    jne     nextfollowers           ;if not, try next second number
+    jne     nextstarter             ;if not, try next starter
     mov     r8d, eax                ;copy of eax to save it
     xor     esi, esi                ;reset esi
 
@@ -76,9 +71,9 @@ getdigits:
 permcheck:
     mov     dil, [digits + esi]             ;get digits[esi]
     cmp     byte [digits + esi + 10], dil   ;must be same as digits[esi + 10]
-    jne     nextfollowers                   ;if not, try next followers
+    jne     nextstarter                     ;if not, try next starter
     cmp     byte [digits + esi + 20], dil   ;same for digits[esi + 20]
-    jne     nextfollowers
+    jne     nextstarter
     inc     esi                             ;next index
     cmp     esi, 10                         ;finished?
     jl      permcheck                       ;if not, repeat
