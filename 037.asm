2a945fcfc06dd763d2b4750b35719d90abc61e99
INIT-REF <rufmichan@freenet.de>
2024-05-24 07:49:41 +0200

    Update

--- 037.asm
+++ 037.asm
@@ -1,80 +1,81 @@
 section .data
-    msg db "%lld", 10, 0        ;return string for printf (just the result)
-    primes times 1000000 db 1   ;array for the prime sieve
+    msg db "%d", 10, 0          ;return string for printf (just the result)
+    primes times 125000 db 255  ;array for the prime sieve
 
 section .text
     extern printf
     global main
 
 main:
-    mov     byte [primes], 0        ;set primes[0] = 0
-    mov     byte [primes + 1], 0    ;set primes[1] = 0
-    mov     ebx, 1                  ;array index for outer loop
+    xor     ebx, ebx        ;array index for outer loop
+    btr     [primes], ebx   ;set 0 and 1 to not prime
+    inc     ebx
+    btr     [primes], ebx
 
 sieve_outer:
-    inc     ebx                     ;increase index
-    mov     eax, ebx                ;copy to eax for squaring
-    mul     ebx                     ;square
-    cmp     eax, 1000000            ;check if square is > limit    
-    jg      reset                   ;if it is, jump to reset
-    cmp     byte [primes + ebx], 0  ;check if ebx is not prime
-    je      sieve_outer             ;if not prime, try next number
+    inc     ebx             ;increase index
+    mov     eax, ebx        ;copy to eax for squaring
+    mul     ebx             ;square
+    cmp     eax, 1000000    ;check if square is > limit    
+    jg      reset           ;if it is, jump to reset
+    bt      [primes], ebx   ;check if ebx is not prime
+    jnc     sieve_outer     ;if not prime, try next number
 
 sieve_inner:
-    mov     byte [primes + eax], 0  ;set multiple to not prime
-    add     eax, ebx                ;next multiple
-    cmp     eax, 1000000            ;check if square is <= limit
-    jl      sieve_inner             ;if it is, continue with inner loop
-    jmp     sieve_outer             ;if not, continue with outer loop
+    btr     [primes], eax   ;set multiple to not prime
+    add     eax, ebx        ;next multiple
+    cmp     eax, 1000000    ;check if square is <= limit
+    jl      sieve_inner     ;if it is, continue with inner loop
+    jmp     sieve_outer     ;if not, continue with outer loop
 
 reset:
-    mov     ebx, 10                 ;10 in ebx (primes < 10 not considered)
-    mov     edi, 10                 ;10 in edi for divisions
-    xor     ecx, ecx                ;sum
-    xor     r8d, r8d                ;counter
+    mov     ebx, 10         ;10 in ebx (primes < 10 not considered)
+    mov     edi, 10         ;10 in edi for divisions
+    xor     ecx, ecx        ;sum
+    xor     r8d, r8d        ;counter
 
 nextprime:
-    inc     ebx                     ;next number
-    cmp     byte [primes + ebx], 1  ;is it prime?
-    jne     nextprime               ;if not, find next prime
-    mov     eax, ebx                ;prime in eax for next step
+    inc     ebx             ;next number
+    bt      [primes], ebx   ;is it prime?
+    jnc     nextprime       ;if not, find next prime
+    mov     eax, ebx        ;prime in eax for next step
 
 trunc_right:
-    xor     edx, edx                ;reset remainder
-    div     edi                     ;divide by 10
-    cmp     byte [primes + eax], 1  ;is truncated number prime?
-    jne     nextprime               ;if not, try next prime
-    cmp     eax, 10                 ;number still >= 10?
-    jge     trunc_right             ;if yes, repeat
-    mov     esi, 1000000            ;1000000 in esi for division
-    mov     eax, esi                ;copy to eax for next step
+    xor     edx, edx        ;reset remainder
+    div     edi             ;divide by 10
+    bt      [primes], eax   ;is truncated number prime?
+    jnc     nextprime       ;if not, try next prime
+    cmp     eax, 10         ;number still >= 10?
+    jge     trunc_right     ;if yes, repeat
+    mov     esi, 1000000    ;1000000 in esi for division
+    mov     eax, esi        ;copy to eax for next step
 
 setdivisor:
-    xor     edx, edx                ;reset remainder
-    div     edi                     ;divide by 10
-    cmp     eax, ebx                ;eax < number?
-    jg      setdivisor              ;if not, repeat
-    mov     esi, eax                ;else set esi
-    mov     eax, ebx                ;prime in eax
+    xor     edx, edx        ;reset remainder
+    div     edi             ;divide by 10
+    cmp     eax, ebx        ;eax < number?
+    jg      setdivisor      ;if not, repeat
+    mov     esi, eax        ;else set esi
+    mov     eax, ebx        ;prime in eax
 
 trunc_left:
-    xor     edx, edx                ;reset remainder
-    div     esi                     ;divide by esi
-    mov     eax, edx                ;remainder in eax
-    cmp     byte [primes + eax], 1  ;is remainder prime?
-    jne     nextprime               ;if not try next prime
-    push    rax                     ;result on the stack
-    mov     eax, esi                ;esi in eax
-    xor     edx, edx                ;reset remainder
-    div     edi                     ;divide by 10
-    mov     esi, eax                ;result back in esi
-    pop     rax                     ;number back from the stack
-    cmp     eax, 10                 ;number still >= 10?
-    jge     trunc_left              ;if yes, repeat
-    add     ecx, ebx                ;else add prime to sum
-    inc     r8d                     ;increase counter
-    cmp     r8d, 11                 ;are all 11 candidates found?
-    jl      nextprime               ;if not, try next prime
+    xor     edx, edx        ;reset remainder
+    div     esi             ;divide by esi
+    mov     eax, edx        ;remainder in eax
+    bt      [primes], eax   ;is remainder prime?
+    jnc     nextprime       ;if not try next prime
+    push    rax             ;result on the stack
+    mov     eax, esi        ;esi in eax
+    xor     edx, edx        ;reset remainder
+    div     edi             ;divide by 10
+    mov     esi, eax        ;result back in esi
+    pop     rax             ;number back from the stack
+    cmp     eax, 10         ;number still >= 10?
+    jge     trunc_left      ;if yes, repeat
+    add     ecx, ebx        ;else add prime to sum
+    inc     r8d             ;increase counter
+    cmp     r8d, 11         ;are all 11 candidates found?
+    jl      nextprime       ;if not, try next prime
 
 print:                  ;printing routine, differs slightly from OS to OS
     push    rbp
