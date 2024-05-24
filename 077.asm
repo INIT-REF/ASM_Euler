d6f411e39522fffb1553b0dc7ce3e498a0b62aaf
INIT-REF <rufmichan@freenet.de>
2024-05-24 08:08:06 +0200

    Update

--- 077.asm
+++ 077.asm
@@ -1,7 +1,7 @@
 section .data
     msg db "%d", 10, 0      ;return string for printf (just the result)
     limit equ 100           ;limit
-    sieve times limit db 1  ;prime sieve
+    sieve times 25 db 255   ;prime sieve
 
 section .bss
     sums resd limit         ;array of sums
@@ -11,36 +11,36 @@ section .text
     global main
 
 main:
-    mov     dword [sums], 1         ;set sums[0] to 1
-    mov     ebx, 1                  ;array index for outer loop
+    mov     dword [sums], 1 ;set sums[0] to 1
+    mov     ebx, 1          ;array index for outer loop
     xor     ecx, ecx
 
 sieve_outer:
-    inc     ebx                     ;increase index
-    mov     eax, ebx                ;copy to eax for squaring
-    mul     ebx                     ;square
-    cmp     eax, limit              ;check if square is > limit    
-    jg      reset                   ;if it is, jump to reset
-    cmp     byte [sieve + ebx], 0   ;check if ebx is no prime
-    je      sieve_outer             ;if no prime, try next number
+    inc     ebx             ;increase index
+    mov     eax, ebx        ;copy to eax for squaring
+    mul     ebx             ;square
+    cmp     eax, limit      ;check if square is > limit    
+    jg      reset           ;if it is, jump to reset
+    bt      [sieve], ebx    ;check if ebx is no prime
+    jnc     sieve_outer     ;if no prime, try next number
 
 sieve_inner:
-    mov     byte [sieve + eax], 0   ;set multiple to not prime
-    add     eax, ebx                ;next multiple
-    cmp     eax, limit              ;check if multiple is <= limit
-    jl      sieve_inner             ;if it is, continue with inner loop
-    jmp     sieve_outer             ;if not, continue with outer loop
+    btr     [sieve], eax    ;set multiple to not prime
+    add     eax, ebx        ;next multiple
+    cmp     eax, limit      ;check if multiple is <= limit
+    jl      sieve_inner     ;if it is, continue with inner loop
+    jmp     sieve_outer     ;if not, continue with outer loop
 
 reset:
-    mov     eax, 1                  ;reset eax
+    mov     eax, 1          ;reset eax
 
 sum_outer:
-    inc     eax                     ;next number
-    cmp     eax, limit              ;limit reached?
-    je      finished                ;if yes, get result
-    cmp     byte [sieve + eax], 1   ;is it prime?
-    jne     sum_outer               ;if not, increase it
-    mov     ebx, eax                ;prime in ebx
+    inc     eax             ;next number
+    cmp     eax, limit      ;limit reached?
+    je      finished        ;if yes, get result
+    bt      [sieve], eax    ;is it prime?
+    jnc     sum_outer       ;if not, increase it
+    mov     ebx, eax        ;prime in ebx
 
 sum_inner:
     mov     edx, ebx                ;copy ebx to edx
