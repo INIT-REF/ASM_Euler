0d979cf8a44448044628e773c1d0b4a9ebdf284e
INIT-REF <rufmichan@freenet.de>
2024-06-05 13:44:43 +0200

    Update

---
+++ 091.asm
@@ -0,0 +1,78 @@
+format ELF64 executable 9
+
+segment readable writable
+    result: times 10 db 0       ;empty string for printing the result later
+                     db 10, 0
+
+segment readable executable
+    entry start
+
+start:
+    mov     ecx, 7500       ;50 * 50 * 3 "trivial" cases
+    xor     edi, edi        ;outer loop index
+
+next_a:
+    inc     edi
+    cmp     edi, 50         ;a > limit?
+    jg      finished        ;if yes, we are finished
+    xor     esi, esi        ;inner loop index
+
+next_b:
+    inc     esi
+    mov     eax, edi
+    mov     ebx, esi
+    call    gcd             ;get gcd(a, b)
+    push    rbx             ;and put it on the stack
+    imul    ebx, esi        ;gcd * b
+    mov     eax, ebx
+    xor     edx, edx
+    div     edi             ;divide by a
+    mov     r8d, eax        ;store result in r8d        
+    pop     rbx             ;gcd back from the stack
+    mov     eax, 50
+    sub     eax, edi        ;50 - a
+    mul     ebx
+    xor     edx, edx
+    div     esi             ;(50 - a) * gcd(a, b) / b
+    cmp     eax, r8d        ;get minimum of eax and r8d
+    cmovg   eax, r8d        ;and put it in eax
+    shl     eax, 1          ;double that
+    add     ecx, eax        ;and add to count
+    cmp     esi, 50         ;b > limit?
+    jl      next_b          ;if not, continue inner loop
+    jmp     next_a          ;else jump to outer loop
+
+gcd:
+    xchg    eax, ebx        ;get gcd(m, n), result in ebx
+    xor     edx, edx
+    div     ebx
+    mov     eax, edx
+    test    eax, eax
+    jnz     gcd
+    ret
+
+finished:
+    mov     eax, ecx        ;convert result to string, print and exit
+    mov     rbx, 10
+    mov     rcx, 9
+
+convert_result:
+    xor     rdx, rdx
+    div     rbx
+    add     rdx, '0'
+    mov     [result + ecx], dl
+    dec     rcx
+    test    rax, rax
+    jnz     convert_result
+
+print:
+    mov     rax, 4
+    mov     rdi, 1
+    mov     rsi, result
+    mov     rdx, 12
+    syscall
+
+exit:
+    mov     rax, 1
+    xor     rdi, rdi
+    syscall
