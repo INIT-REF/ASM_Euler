8d317d4de3740dbf163063edc7b98a765cd57c02
INIT-REF <rufmichan@freenet.de>
2024-06-12 12:45:53 +0200

    Update

--- 073.asm
+++ 073.asm
@@ -3,62 +3,38 @@ format ELF64 executable 9
 segment readable writable
     result: times 10 db 0       ;empty string for printing the result later
                      db 10, 0
-    sieve: rd 12001             ;array for (limit + 1) integers
 
 segment readable executable
     entry start
 
 start:
-    mov     edi, 3              ;init bounding fractions 1/3 and 1/2
-    mov     esi, 2
-    xor     ebx, ebx            ;loop counter
+    mov     ebx, 3              ;b = 3 (denominator of 1/3)
+    mov     ecx, 2              ;d = 2 (denominator of 1/2)
 
-sieve_outer:
-    inc     ebx
-    cmp     ebx, 12000
-    jg      finished
-    mov     eax, ebx            ;get ceil(ebx/2)
-    xor     edx, edx
-    div     esi
-    test    edx, edx
-    jz      no_ceil1
-    inc     eax
-    
-no_ceil1:
-    mov     ecx, eax
-    mov     eax, ebx            ;get ceil(ebx/3)
-    xor     edx, edx
-    div     edi
-    test    edx, edx
-    jz      no_ceil2
-    inc     eax
-
-no_ceil2:
-    sub     ecx, eax
-    dec     ecx                     ;ecx = ceil(ebx/2) - ceil(ebx/3) - 1
-    add     [sieve + 4 * ebx], ecx  ;add result to sieve[ebx]
-    mov     ecx, ebx                ;init inner loop index ecx to 2 * ebx
-    shl     ecx, 1
-    cmp     ecx, 12000              ;ecx > limit?
-    jg      sieve_outer             ;if yes, start over with outer loop
-
-sieve_inner:
-    mov     edx, [sieve + 4 * ebx]  ;subtract sieve[ebx] from sieve[ecx]
-    sub     [sieve + 4 * ecx], edx
-    add     ecx, ebx                ;increase index
-    cmp     ecx, 12000              ;and repeat while < limit
-    jle     sieve_inner
-    jmp     sieve_outer
- 
-finished:
+get_next_frac:
+    mov     edi, ecx            ;calculate median q = (c + d)
+    add     ecx, ebx            ;while q <= 12000
+    cmp     ecx, 12000
+    jle     get_next_frac
+    mov     ecx, edi
     xor     edi, edi
-    xor     eax, eax
 
-sum:                                ;sum of all elements in sieve = result
+get_count:
     inc     edi
-    add     eax, [sieve + 4 * edi]
-    cmp     edi, 12000
-    jl      sum
+    mov     eax, 12000
+    add     eax, ebx
+    xor     edx, edx
+    div     ecx                 ;k = (n + b) / d
+    push    rcx                 ;current d on the stack
+    mul     ecx
+    sub     eax, ebx
+    mov     ecx, eax            ;new d = k * d - b
+    pop     rbx                 ;b = old d
+    cmp     ecx, 2              ;d still > 2?
+    jg      get_count           ;if yes, repeat
+    
+finished:
+    mov     eax, edi
     mov     ebx, 10
     mov     ecx, 9    
 
