faeb1b889020e1e7d01272fb2d907a95fa7b9c1c
INIT-REF <rufmichan@freenet.de>
2024-06-13 08:59:08 +0200

    Update

--- 010.asm
+++ 010.asm
@@ -8,40 +8,38 @@ section .text
 
 main:
     mov     ebx, 1          ;array index for outer loop
+    xor     ecx, ecx        ;sum
 
 sieve_outer:
     inc     ebx             ;increase index
     mov     eax, ebx        ;copy to eax for squaring
     mul     ebx             ;square
     cmp     eax, 2000000    ;check if square is > limit    
-    jg      reset           ;if it is, jump to reset
+    jg      sum             ;if it is, sum rest of primes
     bt      [primes], ebx   ;check if ebx is no prime
     jnc     sieve_outer     ;if no prime, try next number
+    add     rcx, rbx        ;if yes, add prime to sum
 
 sieve_inner:
     btr     [primes], eax   ;set multiple to not prime
     add     eax, ebx        ;next multiple
     cmp     eax, 2000000    ;check if multiple is <= limit
-    jl      sieve_inner     ;if it is, continue with inner loop
+    jle     sieve_inner     ;if it is, continue with inner loop
     jmp     sieve_outer     ;if not, continue with outer loop
 
-reset:
-    mov     rbx, 1          ;reset rax and rbx for building the sum
-    xor     rax, rax
-
 sum:
+    xor     eax, eax        ;zero eax
     inc     ebx             ;increase array index
-    cmp     ebx, 2000000    ;check if index has arrived ad 2,000,000
-    je      print           ;if yes, print result
     bt      [primes], ebx   ;check if primes[ebx] is 1
-    jnc     sum             ;if not, continue
-    add     rax, rbx        ;if yes, add prime to sum
-    jmp     sum             ;back to sum
+    cmovc   eax, ebx        ;if yes, put prime in eax
+    add     rcx, rax        ;and add prime (or 0) to sum
+    cmp     ebx, 2000000    ;end of array?
+    jl      sum             ;if not, repeat
     
 print:                  ;printing routine, differs slightly from OS to OS
     push    rbp
     mov     edi, msg
-    mov     rsi, rax
+    mov     rsi, rcx
     call    printf
     pop     rbp
 
