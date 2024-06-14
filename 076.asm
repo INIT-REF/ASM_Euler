ac3ac1b919d3421928ca1e421beff6f1e3060af8
INIT-REF <rufmichan@freenet.de>
2024-06-14 08:45:11 +0200

    Update

--- 076.asm
+++ 076.asm
@@ -1,52 +1,55 @@
-section .data
-    msg db "%d", 10, 0  ;return string for printf (just the result)
+format ELF64 executable 9
 
-section .bss
-    nums resd 99
-    sums resd 101 ;array of sums, result will be in sums[100]
+segment readable writable
+    result: times 20 db 0       ;empty string for printing the result later
+                     db 10, 0
+    p: rd 101
 
-section .text
-    extern printf
-    global main
+segment readable executable
+    entry start
 
-main:
-    mov     dword [sums], 1         ;set sums[0] to 1
-    xor     eax, eax                ;nums index
-
-setnums:
-    lea     ebx, [eax + 1]          ;set nums[i] = i+1
-    mov     [nums + 4 * eax], ebx
-    inc     eax
-    cmp     eax, 99
-    jl      setnums
-    xor     eax, eax                ;reset eax
+start:
+    mov     dword [p], 1        ;set p[0] to 1
+    xor     edi, edi            ;init n
 
 sum_outer:
-    mov     ebx, [nums + 4 * eax]   ;nums[eax] in ebx
+    inc     edi                 ;n++
+    mov     esi, edi            ;m = n                
 
 sum_inner:
-    mov     edx, ebx                ;copy ebx to edx
-    sub     edx, [nums + 4 * eax]   ;subtract nums[eax]
-    mov     edi, [sums + 4 * edx]   ;move result in edi
-    add     [sums + 4 * ebx], edi   ;add to sums[ebx]
-    inc     ebx                     ;inc inner loop index
-    cmp     ebx, 100                ;end of sums array?
-    jle     sum_inner               ;if lower, continue inner loop
-    inc     eax                     ;else increase outer loop index
-    cmp     eax, 99                 ;end of nums array?
-    jl      sum_outer               ;if lower, start over with outer loop
-    
+    push    rsi
+    sub     esi, edi                ;m - n
+    mov     eax, [p + 4 * esi]      ;p[m - n]
+    pop     rsi
+    add     [p + 4 * esi], eax      ;p[m] += p[m - 1]
+    inc     esi                     ;m++
+    cmp     esi, 100                ;end of p?
+    jle     sum_inner               ;if not, continue inner loop
+    cmp     edi, 99
+    jl      sum_outer
 
-print:                      ;printing routine, differs slightly from OS to OS
-    push    rbp
-    mov     edi, msg
-    mov     dword esi, [sums + 4 * 100]
-    call    printf
-    pop     rbp
+finished:
+    mov     eax, [p + 400]          ;result in p[100]
+    mov     ebx, 10
+    mov     ecx, 19
 
-exit:                       ;exit routine, dito
+convert_result:
+    xor     edx, edx
+    div     ebx
+    add     edx, '0'
+    mov     [result + rcx], dl
+    dec     ecx
+    test    eax, eax
+    jnz     convert_result
+
+print:
+    mov     eax, 4
+    mov     edi, 1
+    mov     esi, result
+    mov     edx, 22
+    syscall
+
+exit:
     mov     eax, 1
     xor     edi, edi
     syscall
-
-section .note.GNU-stack     ;just for gcc
